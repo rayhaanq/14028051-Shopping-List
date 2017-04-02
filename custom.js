8929e18b6ff7b428ac56652be0c171aa77b38863
Rayhaan Quazi <rayhaan.quazi@gmail.com>
2017-04-02 16:29:06 +0100

    Can now edit list items

--- custom.js
+++ custom.js
@@ -29,8 +29,20 @@ $(document).on("pageinit", "#pcreate_list", function(){
 
   });
 
+  $( "#add_item_popup" ).on( "popupafterclose", function( event, ui ) {
+
+    $( "#add_item_popup" ).find("#addItemTitle").text("Add item");
+    $( "#add_item_popup" ).find("#itemNameField").val("");
+    $( "#add_item_popup" ).find("#qtyField").val("");
+    $( "#add_item_popup" ).find("#addItemSubmit").text("Add");
+    $( "#add_item_popup" ).find("#addItemSubmit").attr('onclick', 'addItem()');
+
+  } );
+
 
 });
+
+
 
 function addItem(){
   itemName = $("#itemNameField").val().trim();
@@ -70,12 +82,72 @@ function generateItemList(){
   //var itemIdNumber = 0;
   for (var item in chosenItems){
 
-    output += "<li><a href='#' >" + item + "<span class='ui-li-count'>x" + chosenItems[item] + "</span></a><a id='" + item + "' href='#confirm_del_popup' data-rel='popup' onclick='setObjectToDelete(this)'></a></li>";
+    output += "<li><a class='" + item + "' href='#' onclick='prepareEditItem(this)'>" + item + "<span class='ui-li-count'>x" + chosenItems[item] + "</span></a><a class='" + item + "' href='#confirm_del_popup' data-rel='popup' onclick='setObjectToDelete(this)'></a></li>";
 
     //itemIdNumber += 1;
   }
   $("#itemList").append(output);
   $("#itemList").listview("refresh");
+}
+
+itemToEdit = 0;
+
+function prepareEditItem(obj){
+
+  $( "#add_item_popup" ).find("#addItemTitle").text("Edit item");
+  $( "#add_item_popup" ).find("#addItemSubmit").text("Save");
+  $( "#add_item_popup" ).find("#itemNameField").val(getItemName(obj));
+  $( "#add_item_popup" ).find("#qtyField").val(chosenItems[getItemName(obj)]);
+  $( "#add_item_popup" ).popup("open");
+  $( "#add_item_popup" ).find("#addItemSubmit").attr('onclick', 'editItem()');
+  itemToEdit = getItemName(obj);
+
+}
+
+function editItem (){
+  itemName = $("#itemNameField").val().trim();
+  quantity = $("#qtyField").val();
+
+  if (!itemName || !quantity || quantity <= 0) {
+
+        if (quantity <= 0){
+
+          $("#invalidItem").text("Quantity must be 1 or more");
+        } else {
+          $("#invalidItem").text("Please fill all fields");
+        }
+
+  } else {
+
+    if (itemName == itemToEdit){
+      chosenItems[itemName] = quantity;
+    } else {
+      delete chosenItems[itemToEdit];
+      chosenItems[itemName] = quantity;
+    }
+    $( "#add_item_popup" ).popup("close");
+
+    // $("#itemList").append("<li><a href='#'>" + itemName + "<span class='ui-li-count'>x" + quantity + "</span></a><a href='#'></a></li>");
+    // $("#itemList").listview("refresh");
+    generateItemList();
+
+    $("#itemNameField").val("");
+    $("#qtyField").val("");
+
+    alert(JSON.stringify(chosenItems));
+  }
+
+
+
+}
+
+
+function getItemName(obj){
+  var classes= $(obj).attr("class");
+
+  classes = classes.substring(0, classes.indexOf(' '));
+
+  return classes;
 }
 
 objToDelete = 0;
@@ -84,8 +156,10 @@ function deleteItem() {
 
 
   $(objToDelete).closest("li").remove();
-  delete chosenItems[$(objToDelete).attr("id")];
+  delete chosenItems[getItemName(objToDelete)];
+  alert(JSON.stringify(chosenItems));
   $('#confirm_del_popup').popup('close');
+
 
 }
 
